063e85142dfe9a83ab1a31416d81cbabde66d419
Matthieu Muller <mullerm1002@gmail.com>
2021-09-21 21:29:30 +0200

    Restaurant controller added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'belgian' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London', category: 'italian' }
+pizza_west = { name: 'Pizza West', address: '240B Greenwich, London', category: 'italian' }
+pizza_north = { name: 'Pizza North', address: '15C Bloomsbury, London', category: 'italian' }
+pizza_south = { name: 'Pizza South', address: '5A Camberwell, London', category: 'italian' }
+
+[dishoom, pizza_east, pizza_west, pizza_north, pizza_south].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
