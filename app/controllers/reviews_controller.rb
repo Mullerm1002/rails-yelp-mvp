ee7c5f5d5dba840524578deb783b05302af2ded6
Matthieu Muller <mullerm1002@gmail.com>
2021-09-22 16:48:54 +0200

    Review Controller create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,9 @@
 class ReviewsController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
 
   def create
     @review = Review.new(review_params)
